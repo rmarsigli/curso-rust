cd7fd01a0f6fc099a927a90ab98a5c668c45c238
Rafhael Marsigli <rmarsigli@gmail.com>
2026-01-03 22:56:29 -0300

    fix: remove wrong path in tasks router

--- src/routes/tasks.rs
+++ src/routes/tasks.rs
@@ -4,9 +4,9 @@ use crate::handlers::tasks::SharedState;
 
 pub fn router() -> Router<SharedState> {
     Router::new()
-        .route("/tasks/{id}", delete(delete_task))
-        .route("/tasks/{id}", patch(update_task))
-        .route("/tasks/{id}", get(get_task))
-        .route("/tasks", post(create_task))
-        .route("/tasks", get(list_tasks))
+        .route("/{id}", delete(delete_task))
+        .route("/{id}", patch(update_task))
+        .route("/{id}", get(get_task))
+        .route("/", post(create_task))
+        .route("/", get(list_tasks))
 }
